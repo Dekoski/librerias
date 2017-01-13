80ffbcbe1ca801b5fea5a1189adb309a03ca1730
opm <odelapuente@gmail.com>
2017-01-13 11:30:22 +0100

    reloj

---
+++ reloj.sh
@@ -0,0 +1,5 @@
+while true; do
+    #tput sc; tput cup 0 0; date +%r | toilet -f bigmono12 -F border --gay ; tput rc; sleep 1;
+    #tput sc; tput cup 0 0; date +%r | toilet -f s-relief ; tput rc; sleep 1;
+    tput sc; tput cup 0 0; date +%r | toilet -f fraktur -w 120; tput rc; sleep 1;
+done
